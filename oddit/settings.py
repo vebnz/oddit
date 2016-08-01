a2063fe7bdc53f5b8cdaecac42e7a13492ee6e4d
Jamie Gracie <jamie@gracie.kiwi>
2016-08-02 01:10:35 +1200

    FIXED: Hander now requires filter setting

--- oddit/settings.py
+++ oddit/settings.py
@@ -171,6 +171,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+			'filters': [],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
