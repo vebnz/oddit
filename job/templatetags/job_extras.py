cb36b7c918bc71136b84db998da848f31b0047b3
Mike Mackenzie <mike@veb.co.nz>
2016-08-08 17:05:17 -0400

    added templatetags for custom tags esp for gravtar

---
+++ job/templatetags/job_extras.py
@@ -0,0 +1,20 @@
+import hashlib
+import urllib
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+# return only the URL of the gravatar
+# TEMPLATE USE:  {{ email|gravatar_url:150 }}
+@register.filter
+def gravatar_url(email, size=40):
+    default = "http://placehold.it/300x300"
+    return "https://www.gravatar.com/avatar/%s?%s" % (hashlib.md5(email.lower()).hexdigest(), urllib.urlencode({'d':default, 's':str(size)}))
+
+# return an image tag with the gravatar
+# TEMPLATE USE:  {{ email|gravatar:150 }}
+@register.filter(name='gravatar')
+def gravatar(email, size=40):
+    url = gravatar_url(email, size)
+    return mark_safe('<img src="%s" height="%d" width="%d">' % (url, size, size))
