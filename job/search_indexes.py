67ca2019f259115708caa70f90a17972723529c4
Mike Mackenzie <mike@veb.co.nz>
2016-08-09 06:17:56 -0400

    haystack search searches titles and company fields

--- job/search_indexes.py
+++ job/search_indexes.py
@@ -12,7 +12,8 @@ class JobIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(created__lte=datetime.datetime.now())
+        #return self.get_model().objects.filter(created__lte=datetime.datetime.now())
+        return self.get_model().objects.all()
 
 class CompanyIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
