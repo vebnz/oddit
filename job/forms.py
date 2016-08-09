bc02b6b4a378374bee460dac0e02e6f1d16d7d1c
Jamie Gracie <jamie@gracie.kiwi>
2016-08-09 21:46:44 +1200

    FIXED: Max length for Title and Company
    closes #95

--- job/forms.py
+++ job/forms.py
@@ -26,8 +26,8 @@ class JobForm(forms.ModelForm):
             return instance.save()
 
     # override anything here
-    company = CharField(label='Company Name')
-    title = CharField(label='Job Title')
+    company = CharField(label='Company Name',max_length=30)
+    title = CharField(label='Job Title',max_length=30)
     budget = CharField(label='Budget ($)')
     class Meta:
         model = Job
