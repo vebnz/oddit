a550d23ddf8225076cb0ca5e04d23b78d7d896f7
Jamie Gracie <jamie@gracie.kiwi>
2016-08-12 20:38:55 +1200

    ADDED: Relation between user and profile

--- job/views.py
+++ job/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.exceptions import ObjectDoesNotExist
-from job.forms import JobForm, ApplyForm, ProfileForm
+from job.forms import JobForm, ApplyForm, UserProfileForm
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
@@ -342,15 +342,15 @@ def settings(request):
     popular_tags = Tag.objects.usage_for_model(Job, counts=True)[:5]
     popular_tags.sort(key=operator.attrgetter('count'), reverse=True)
     category_list = Category.objects.all()[:10]
-    
+
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user)
+        form = UserProfileForm(request.POST, instance=request.user.profile)
         form.user = request.user
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/my-profile')
     else:
-        form = ProfileForm(instance=request.user)
+        form = UserProfileForm(instance=request.user.profile)
 
     return render_to_response('jobs/settings.html',  {
         'form' : form,
