02387eeef1565979202b533a99ce8f3090f32e82
Ray Arvin Rimorin <rayarvin@gmail.com>
2016-08-15 17:28:58 +0800

    ping pong save to database

--- notification/routing.py
+++ notification/routing.py
@@ -1,8 +1,9 @@
 from channels.routing import route
-from oddit.consumers import ws_message, ws_connect, ws_disconnect
+from notification.consumers import ws_message, ws_connect, ws_disconnect, msg_consumer
 
 channel_routing = [
     route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
     route("websocket.disconnect", ws_disconnect),
+    route("lobby-messages", msg_consumer)
 ]
