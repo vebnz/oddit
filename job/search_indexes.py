708c9920d8661dd8e18a1fd033f069391cdb6ab8
Jamie Gracie <jamie@gracie.kiwi>
2016-08-15 20:08:07 +1200

    REMOVED: Company

--- job/search_indexes.py
+++ job/search_indexes.py
@@ -1,11 +1,10 @@
 import datetime
 from haystack import indexes
-from job.models import Job, Company
+from job.models import Job
 
 class JobIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    company = indexes.CharField(model_attr='company')
 
     def get_model(self):
         return Job
@@ -14,15 +13,3 @@ class JobIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         #return self.get_model().objects.filter(created__lte=datetime.datetime.now())
         return self.get_model().objects.all()
-
-class CompanyIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name')
-
-    def get_model(self):
-        return Company
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(created__lte=datetime.datetime.now())
-
